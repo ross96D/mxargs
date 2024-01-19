550fbcd1dcb24c4aa27163e966faeb2f0454e261
ross96D <abelaross96@gmail.com>
2024-01-18 20:22:07 -0500

    fix: number of goroutines spwaned

--- execute/execute.go
+++ execute/execute.go
@@ -1,10 +1,14 @@
 package execute
 
 import (
+	"context"
 	"io"
 	"os"
 	"os/exec"
+	"runtime"
 	"sync"
+
+	"golang.org/x/sync/errgroup"
 )
 
 type Print struct {
@@ -19,19 +23,32 @@ func (p *Print) print(r io.Reader) {
 
 func Execute(print *Print, cmd *exec.Cmd, args []string) {
 	w := sync.WaitGroup{}
+
+	ctx := context.Background()
+	g, _ := errgroup.WithContext(ctx)
+	g.SetLimit(runtime.NumCPU())
+
 	for i := 0; i < len(args); i++ {
 		w.Add(1)
 		cmd2 := *cmd
 		cmd2.Args = append(cmd2.Args, args[i])
-		go func(cmd exec.Cmd) {
-			r, _ := cmd.StdoutPipe()
-			go func() {
-				cmd.Run()
-				r.Close()
-			}()
-			print.print(r)
-			w.Done()
-		}(cmd2)
+
+		g.Go(func(cmd exec.Cmd) func() error {
+			return func() error {
+				r, _ := cmd.StdoutPipe()
+				go func() {
+					cmd.Run()
+					r.Close()
+				}()
+				print.print(r)
+				w.Done()
+				return nil
+			}
+		}(cmd2))
+
+		// go func(cmd exec.Cmd) {
+
+		// }(cmd2)
 	}
 	w.Wait()
 }
